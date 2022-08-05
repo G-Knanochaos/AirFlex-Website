2db195030e0ab6e7432b4467a1b13167d5bc6061
Christopher Vu <107224696+ChrisMV2007@users.noreply.github.com>
2022-08-04 19:50:49 -0700

    Bux fix + access json

--- website/views.py
+++ website/views.py
@@ -2,47 +2,54 @@ from flask import Blueprint, redirect, url_for, render_template, request, jsonif
 from flask_login import login_required, current_user
 from .models import ACdatum
 from . import db
+from .Backend_Scripts import AC_Calc
+from .Backend_Scripts.AC_Calc import input_request as inp
 
-views = Blueprint('views',__name__)
+views = Blueprint('views', __name__)
+
+recent_bill_iter = []
+
 
 @views.route('')
 def default():
-    return render_template("home.html",user=current_user)
+    return render_template("home.html", user=current_user)
+
 
 @views.route('home')
 def home():
     return redirect(url_for("views.default"))
-    #redirects to default page for people who search url/home
+    # redirects to default page for people who search url/home
+
 
 @views.route('base')
 @login_required
 def webflow():
-    return render_template("base2.html",user=current_user)
+    return render_template("base2.html", user=current_user)
 
-@views.route('aircalculator', methods = ['GET','POST'])
+
+@views.route('aircalculator', methods=['GET', 'POST'])
 @login_required
 def aircalculator():
-    if request.method == 'POST': #this checks the request type
-        '''
-        #example of how to make new ACdata instance
-        hours, temp, bill = some_backend_function(request.form)
-        new_ACdatum = ACdatum(hours=hours,temp=temp,estimated_bill=bill)
-        db.session.add(new_ACdatum)
-        db.session.commit()
-        '''
-        #return request.form
+    global recent_bill_iter
+    if request.method == 'POST':
+        KwH, GOT = AC_Calc.KwH(inp('BTU_rating'), inp('EER'), inp('wattage'), [inp('size'), inp('type')])
+        res_iter = AC_Calc.Price(KwH, inp('hours'), inp('temp'), inp('state'), inp('use-average'),
+                                  inp('major-city'), inp('month'), inp('day-avg-temp'), inp('day-high-temp'),
+                                  inp('save'))
+        global recent_bill_iter
+        recent_bill_iter = res_iter
         if request.form.get("save"):
-            
-            new_ACdatum = ACdatum(hours=request.form.get("BTU_rating"),\
-                                  temp=request.form.get("EER"),\
-                                  estimated_bill=request.form.get("temp"),\
-                                  user_id = current_user.id) #associates data with current_user
-            db.session.add(new_ACdatum) #adds data to db session
-            db.session.commit() #commits data 
-        return render_template("aircalculator.html",user=current_user) #this is what will be returned to the page
-    return render_template("aircalculator.html",user=current_user)
+            new_ACdatum = ACdatum(hours=res_iter[1],
+                                  temp=res_iter[2],
+                                  estimated_bill=res_ter[0],
+                                  user_id=current_user.id)
+            db.session.add(new_ACdatum)
+            db.session.commit()
+        return render_template("aircalculator.html", user=current_user)  # this is what will be returned to the page
+    return render_template("aircalculator.html", user=current_user)
 
-@views.route('next_question', methods = ['GET', 'POST'])
+
+@views.route('next_question', methods=['GET', 'POST'])
 def next_question():
     a = request.args.get('a', 0, type=int)
     b = request.args.get('b', 0, type=int)
