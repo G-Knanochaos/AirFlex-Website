8a0176fa69379e8e26da5d989309581127f659a1
Gavin-kai Vida <73557277+G-Knanochaos@users.noreply.github.com>
2022-08-05 14:12:11 -0700

    New Updatre
    - Added AC tracker page to display user data (graphs, how much you have saved in total, etc.)
    - Added temporary results for when user doesn't want to save data

--- website/views.py
+++ website/views.py
@@ -34,44 +34,45 @@ def aircalculator():
     global recent_bill_iter
     if request.method == 'POST':
         KwH, GOT = AC_Calc.KwH(inp('BTU_rating'), inp('EER'), inp('wattage'), [inp('size'), inp('type')])
+        '''
         res_iter = AC_Calc.Price(KwH, inp('EER'), inp('hours'), inp('temp'), inp('state'), inp('use-average'),
                                  inp('major-city'), inp('month'), inp('day-avg-temp'), inp('day-high-temp'),
                                  inp('save'))
+        '''
+        res_iter = [123.0, 3, 69]
         global recent_bill_iter
         recent_bill_iter = res_iter
-        if request.form.get("save"):
+        if request.form.get("save") == "1":
             new_ACdatum = ACdatum(hours=res_iter[1],
                                   temp=res_iter[2],
                                   estimated_bill=res_iter[0],
                                   user_id=current_user.id)
             db.session.add(new_ACdatum)
             db.session.commit()
-        return res_iter  # this is what will be returned to the page
-    return render_template("aircalculator.html", user=current_user)
+            return redirect(url_for("views.actracker")) #if user wants to save data, redirected to actracker page
+        else:
+            return render_template("aircalculator.html",user=current_user,display=1,results =res_iter) #if user does not want to save data, shows temporary results div
+    return render_template("aircalculator.html",user=current_user,display=0)
 
 
 @views.route('fancalculator', methods=['GET', 'POST'])
 @login_required
 def fancalculator():
     if request.method == 'POST':
-        price = fan_price(inp('state'), inp('type'), inp('wattage'), inp('hours'))
-        if request.form.get("save"):
+        price = round(fan_price(inp('state'), inp('type'), inp('wattage'), inp('hours')),1)
+        if request.form.get("save") == "1":
             new_fanData = FanData(estimated_bill=price,
                                   user_id=current_user.id)
             db.session.add(new_fanData)
             db.session.commit()
-        return render_template("fancalculator.html", user=current_user) # this is what will be returned to the page
-    return render_template("fancalculator.html", user=current_user)
+            return redirect(url_for("views.actracker"))
+        else:
+            return render_template("fancalculator.html", user=current_user, display = 1,bill=price)
+    return render_template("fancalculator.html", user=current_user, display = 0)
 
 @views.route('actracker',methods = ['GET','POST'])
 @login_required
 def actracker():
-    return render_template("actracker.html",user=current_user)
-
-
-
-@views.route('next_question', methods=['GET', 'POST'])
-def next_question():
-    a = request.args.get('a', 0, type=int)
-    b = request.args.get('b', 0, type=int)
-    return jsonify(result=a + b)
+    return render_template("actracker.html",user=current_user,acdata_length = len(current_user.ACdata))
+    
+    
